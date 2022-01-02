a5b23791bf617e932a10d8f57adc376e138402a0
Roshan Ravi <roshan@roshanravi.com>
2022-01-01 21:48:18 -0500

    Update Underlying Data

--- README.md
+++ README.md
@@ -15,11 +15,7 @@
 ## Breakdown
 The codebase is split into three major sections. The first two are solely data cleansing and validating the proof of concept prior to synthesizing the information into a web dashboard.
 
-1. `node filters.js`
-    - This script filters comments.csv and doctor-names.csv into comments-filtered.csv
-2. `node comments.js`
-    - Proof of Concept, Keyword Analysis
-3. `node data-synthesize.js`
+1. `node process.js`
     - Primary script used to generate final JSON presented on site, seen at `index.html`. 
     - This script runs some minor calculations, does keyword analysis and basic sentiment analysis and then outputs `data-synthesized.json`.
 
@@ -31,7 +27,7 @@ The website simply takes `data-synthesized.json` and visualizes the information 
 By uploading to the GitHub repository master branch, the [GitHub Pages](https://pages.github.com) hosting service will automatically update the live website.
 
 ## Adding new Data
-Within `data-synthesize.js`, there's a comment `/* NEW DOCTOR METRICS GO HERE */`. That section is traversed for each doctor and is currently used to join the high-level doctor metadata with individual comments to one consolidated JSON structure for the web page.
+Within `process.js`, there are comments `/* NEW PHYSICIAN METRICS GO HERE */` and `/* NEW COMMENT METRICS GO HERE */`. Those sections is traversed for each physician/comment and is currently used to join the high-level doctor metadata with individual comments to one consolidated JSON structure for the web page.
 
 If needed, more JSON/CSV files can be loaded to the script and joined into the data structure. This allows for easy integration of other approaches of topic modeling, keywords and sentiment analysis.
 
@@ -48,10 +44,3 @@ If needed, more JSON/CSV files can be loaded to the script and joined into the d
         - lmotiwalla.github.io
 
 Please see [GitHub Pages Custom Domains](https://docs.github.com/en/github/working-with-github-pages/configuring-a-custom-domain-for-your-github-pages-site) for more information about how to change the URL in future after domain purchase.
-
-## Proposed TODOs (currently pending)
-- [ ] Percentile Ranking based on Specialty/Composite Score
-    - Top 1%, Top 5%, Top 10%, Top 25%, and Top 50%
-- [ ] Filtering similar to Amazon
-    - Most Recent (Default) / Top Reviews
-- [ ] Welcome Page prior to Table/List View
---
+++ process.js
@@ -0,0 +1,81 @@
+/* Import NPM dependencies */
+const fs = require('fs');
+const parse = require('csv-parse');
+const _ = require('lodash');
+const keyword_analyzer = require('keyword-analyzer');
+
+/* Read & Parse doctor-names.csv */
+fs.readFile('./doctor-names.csv', function (err, fileData) {
+  parse(fileData, {columns: true, trim: true}, function(err, rows) {
+    /* Map 2D names CSV data into 1D name array */
+    const names = _.map(rows, row =>
+      row["physician"].trim().toLowerCase().replace(/[a-z] [a-z] [a-z]/g, ' ').replace(/\s\s+/g, ' ')
+    );
+
+    /* Read & Parse comments.csv */
+    fs.readFile('./comments.csv', function (err, fileData) {
+      parse(fileData, {columns: true, trim: true}, function(err, rows) {
+        /* Filter based on doctor name */
+        const filtered_comments = _.filter(rows, row => 
+            /* Case Insensitive Comparison to Names array */
+            _.includes(names, row["physician"].trim().toLowerCase().replace(/ [a-z] /g, ' ').replace(/\s\s+/g, ' '))
+        ).map(comment => ({
+            physician: comment["physician"],
+            text: comment["text"],
+            patient_rating_scaled: Number(comment["patient_rating_scaled"]),
+            sentiment_rating_unscaled: Number(comment["sentiment_rating_unscaled"]),
+            sentiment_rating_scaled: Number(comment["sentiment_rating_scaled"]),
+            link: comment["link"].replace(/ /g, '.'),
+            /* NEW COMMENT METRICS GO HERE */
+        }));
+
+        /* Print metadata to console output */
+        console.log(`Total: ${rows.length} comments`);
+        console.log(`Working Set: ${names.length} doctors`);
+        console.log(`Working Set: ${filtered_comments.length} comments`);
+
+        fs.readFile('./physicians.csv', function (err, fileData) {
+            parse(fileData, {columns: true, trim: true}, function(err, physicians) {
+                const processed_data = physicians.map((physician, index) => ({
+                    id: index,
+                    name: physician["Name"],
+                    degree: physician["Degree"],
+                    specialty: physician["Specialty"],
+                    gender: physician["Gender"],
+                    age: Number(physician["Age"]),
+                    total_reviews: (Number(physician["Review_No_HG"]) + Number(physician["Review_No_vital"])),
+                    total_score: (Number(physician["New_total_score_HG1"]) * Number(physician["Review_No_HG"]) + Number(physician["Total_score_vital"]) * Number(physician["Review_No_vital"])) / (Number(physician["Review_No_HG"]) + Number(physician["Review_No_vital"])),
+                    healthgrades_score: Number(physician["New_quality_score_HG"]),
+                    vitals_score: Number(physician["New_quality_score_vital"]),
+                    /* NEW PHYSICIAN METRICS GO HERE */
+                })).map(physician => {
+                    physician_comments = _.filter(filtered_comments, comment => (comment["physician"] == physician["name"]));
+
+                    return Object.assign(physician, {
+                        comments: physician_comments,
+                        total_comments: physician_comments.length,
+                        keywords: keyword_analyzer
+                        .wrest(physician_comments.map(comment => comment.text).join(" "), {
+                            stopWords: [
+                                "dr", "", "doctor", "medical", "patient", "doctors", "physician", "office", "patients",
+                                "tales", "health", "call", "told", "questions", "primary", "practice",
+                                "lot", "months", "hours", "days", "pcp", "day", "minutes", "extremely", "people",
+                                "husband", "wife", "mother", "father", "son", "daughter", "hour", "month", "hospital", "week",
+                                "weels", "review", "reviews", "moved", "appt", "loved", "absolutely", "medicine", "family",
+                                "takes", "total", "highly", "offer", "save", "easy"
+                            ].concat(require('stopwords').english)
+                        })
+                        .filter(keyword => keyword.length > 3)
+                        .filter(keyword => !physician["name"].toLowerCase().split(" ").includes(keyword))
+                        .slice(0, 8)
+                        /* NEW COMMENT CALCULATED FIELDS GO HERE */
+                    })
+                });
+                
+                fs.writeFileSync("data-synthesized.json", JSON.stringify(processed_data, null, 4));
+            });
+        });
+      });
+    });
+  });
+});
